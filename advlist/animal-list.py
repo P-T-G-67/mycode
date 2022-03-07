b51f9810f612f4a9c11027bf0abdb5cada77969e
P-T-G-67 <P-T-G-67@users.noreply.github.com>
2022-03-07 22:06:24 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Create a list of animals with three-letter names
+
+ani_list = ["fox", "fly", "bee", "cat", "dog"]
+
+print(ani_list)
+
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#Create a list
+
+#my_list = ["192.168.0.5", 5060, "UP"]
+ 
+##Print the IP Address from our list
+#print("The first item in the list (IP): " + my_list[0] )
+
+##Return the port #
+#print("The second item in the list (port): " + str(my_list[1]) )
+
+##Return the state
+#print("The last item in the list (state): " + my_list[2] )
+
+#Create a new list
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+#Display only IPs
+
+print(iplist[3], iplist[4])
+

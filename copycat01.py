4a97c8758f1a6d9788322f26b5c4a9389d5756d0
P-T-G-67 <P-T-G-67@users.noreply.github.com>
2022-03-08 18:02:03 +0000

    copying local files with python

---
+++ copycat01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Import modules
+import shutil
+import os
+
+# Set the start location of our script
+os.chdir("/home/student/mycode/")
+
+# Copy fileA to fileB
+shutil.copy("5g_research/sdn_network.txt", "5g_research_backup/sdn_network.txt.copy")
+
+# Copy the entire directoryA to directoryB
+shutil.copytree("5g_research/", "5g_research_backup/")

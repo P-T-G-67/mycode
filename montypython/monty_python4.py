96e92d20b2ade067d76a09ee69ae3b995e0cb30b
P-T-G-67 <P-T-G-67@users.noreply.github.com>
2022-03-09 20:24:25 +0000

    While looping

---
+++ montypython/monty_python4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+def main():
+
+    round = 0 # integer round initiated to 0
+    answer = " "
+    while round < 3 and answer != "Brian":        # sets up an infinite loop condition
+        round = round + 1       #increases the round counter
+        print('Finish the movie title, "Monty Python\'s The Life of ______"')
+        answer = input("Your guess--> ")        # string ans collected from user
+        answer = answer.capitalize()
+        if answer == 'Brian':
+            print('Correct!')
+            break       # break statement escapes the while loop
+        elif answer == 'Shrubbery':
+            print('You gave the super secret answer!')
+        elif round==3:        # logic to ensure round has not yet reached 3
+            print("Sorry, the answer was Brian.")
+            break       # break statement escapes the while loop
+        else:            # if answer was wrong, and round is not yet equal to 3
+            print("Sorry!  Try again!")
+main()
+print("Exiting script")
